3ea8b2a8e45fdddc337372e2e88ab5e9d3283928
Tsimpson000 <Tsimpson000@users.noreply.github.com>
2023-06-20 19:32:30 -0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Midterm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Opens new product window
+        private void btnAddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            new AddProduct().Show();
+        } //btnAddProduct_Click
+
+        //Opens new member window
+        private void btnAddMember_Click(object sender, RoutedEventArgs e)
+        {
+            new AddMember().Show();
+        } //btnAddMember_Click
+
+    }
+}
